82fbc7122ce615a22cd488e35ffa006e0ba392b7
dopna <dopnaremy@gmail.com>
2023-11-06 21:14:51 +0100

    Interface_utilisateurt

---
+++ applicationDesktop/connexion.xaml.cs
@@ -0,0 +1,20 @@
+using System.Windows;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;  
+using System.Data.SqlClient;
+
+namespace applicationDesktop;
+
+public partial class connexion : Window
+{
+    public connexion()
+    {
+        InitializeComponent();
+    }
+
+    class Functions
+    {
+        
+    }
+}
